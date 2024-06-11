4d9619bcd519f090028aadff51b3a0a602f75a49
Gary Smith <garysmith193358@gmail.com>
2024-06-11 17:29:34 -0400

    added menu options

---
+++ src/traffic/Main.java
@@ -0,0 +1,12 @@
+package traffic;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the traffic management system!");
+        System.out.println("Menu:");
+        System.out.println("1. Add");
+        System.out.println("2. Delete");
+        System.out.println("3. System");
+        System.out.println("0. Quit");
+    }
+}
